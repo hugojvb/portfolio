86b0a99e553c51380f9352094c1057d34d7ae3ef
Hugo Batista <hugojvb20@gmail.com>
2020-09-29 14:13:10 +0100

    Changed webpackcommon test vids

--- webpack.common.js
+++ webpack.common.js
@@ -2,34 +2,34 @@ const Webpack = require("webpack");
 module.exports = {
   entry: {
     vendor: "./src/vendor.js",
-    main: "./src/index.js"
+    main: "./src/index.js",
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ["html-loader"]
+        use: ["html-loader"],
       },
       {
-        test: /\.(jpg|jpeg|png|gif|svg|pdf)$/,
+        test: /\.(jpg|jpeg|png|gif|svg|pdf|webm|mp4)$/,
         use: [
           {
             loader: "file-loader",
             options: {
               name: "[name].[hash].[ext]",
               outputPath: "assets",
-              esModule: false
-            }
-          }
-        ]
-      }
-    ]
+              esModule: false,
+            },
+          },
+        ],
+      },
+    ],
   },
   // This config allows to use jQuery $ sign
   plugins: [
     new Webpack.ProvidePlugin({
       $: "jquery",
-      jQuery: "jquery"
-    })
-  ]
+      jQuery: "jquery",
+    }),
+  ],
 };
