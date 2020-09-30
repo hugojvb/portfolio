0ef519b713be8ae2e0de6d540595450deefbaf32
Hugo Batista <hugojvb20@gmail.com>
2020-09-30 02:57:09 +0100

    changed heaviest vid

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import initTilt from "./js/tilt";
 import initSr from "./js/sr";
 
 import "./style/main.scss";
-import fitbit from "./assets/fitbit.mp4";
+import fitbit from "./assets/fitbit3.mp4";
 import alexa from "./assets/alexa.mp4";
 
 $('a[href^="#"]').on("click", function (event) {
